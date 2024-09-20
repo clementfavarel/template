e0af7270418a8512db7b80b205ffccecd731f032
clementfavarel <pl.kode.pro@gmail.com>
2024-09-20 17:06:57 +0000

    modified redirection and navbar

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,6 +1,8 @@
 <x-main-layout>
     <div class="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-0 h-[calc(100dvh-80px)]">
         <h1>Accueil</h1>
-        <div>{{ Auth::user()->name }}</div>
+        @auth
+            <div>{{ Auth::user()->name }}</div>
+        @endauth
     </div>
 </x-main-layout>
